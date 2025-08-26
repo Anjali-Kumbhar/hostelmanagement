091b53accecf26b3e623f59a32e479cabb493da9
Anjali-Kumbhar <159118771+Anjali-Kumbhar@users.noreply.github.com>
2025-08-26 21:45:29 +0530

    Delete Hoste-Management-backend directory

--- Hoste-Management-backend/models/OtpVerification.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const otpVerificationSchema = new mongoose.Schema({
-  email: String,
-  otp: String,
-  createdAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model("OtpVerification", otpVerificationSchema);
